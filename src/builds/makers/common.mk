430bbd214070068bcf78ca96c4430360b573817e
Ikenna Okpala <me@ikennaokpala.com>
2020-09-14 08:33:09 -0300

    Install sudo prior to Makefile

--- src/builds/makers/common.mk
+++ src/builds/makers/common.mk
@@ -1,13 +1,11 @@
 .PHONY: build-yum-packages  build-apt-packages build-homebrew-packages
 
 build-yum-packages:
-	yum install sudo -y
 	$(SUDO) yum update -y
 	$(SUDO) yum groupinstall 'Development Tools' -y 
 	$(SUDO) yum install -y sqlite-devel readline-devel curl which wget make openssl-devel bzip2-devel xz-devel libffi-devel zlib-devel python$(LSARP_API_PYTHON_MAIN_VERSION)-devel libevent-devel postgresql-devel -y
 
 build-apt-packages:
-	apt-get install sudo -y --no-install-recommends
 	$(SUDO) apt-get update -y
 	$(SUDO) apt-get install -y libbz2-dev libsqlite3-dev libreadline6-dev libffi-dev python3-dev libjpeg-dev libjpeg8-dev python3-pip python3-psycopg2 git curl wget libssl-dev build-essential liblzma-dev make zlib1g-dev python$(LSARP_API_PYTHON_MAIN_VERSION)-dev libevent-dev libblas-dev libatlas-base-dev python$(LSARP_API_PYTHON_MAIN_VERSION)-venv libpq-dev postgresql-contrib -y --no-install-recommends
 	$(SUDO) apt-get clean
--- src/builds/makers/python.mk
+++ src/builds/makers/python.mk
@@ -7,13 +7,13 @@ build-homebrew-python-packages:
 	python$(LSARP_API_PYTHON_VERSION) -m pip install --upgrade pip pipenv
 
 pyenv-packages:
-	curl https://pyenv.run | bash
+	@if [[ ! -d $(PYENV_ROOT) ]] ; then curl https://pyenv.run | bash; fi	
 	pyenv update
 	pyenv install $(LSARP_API_PYTHON_INSTALL_VERSION)
 	pyenv local $(LSARP_API_PYTHON_INSTALL_VERSION)
 	pyenv global $(LSARP_API_PYTHON_INSTALL_VERSION)
-	$(SUDO) rm -fr $(LSARP_API_PYTHON_BIN_PATH)/python && ln $(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/python$(LSARP_API_PYTHON_MAIN_VERSION) $(LSARP_API_PYTHON_BIN_PATH)/python
-	$(SUDO) rm -fr $(LSARP_API_PYTHON_BIN_PATH)/pip && ln $(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/pip$(LSARP_API_PYTHON_MAIN_VERSION) $(LSARP_API_PYTHON_BIN_PATH)/pip
+	$(SUDO) rm -fr $(LSARP_API_PYTHON_BIN_PATH)/python && $(SUDO) ln $(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/python$(LSARP_API_PYTHON_MAIN_VERSION) $(LSARP_API_PYTHON_BIN_PATH)/python
+	$(SUDO) rm -fr $(LSARP_API_PYTHON_BIN_PATH)/pip && $(SUDO) ln $(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/pip$(LSARP_API_PYTHON_MAIN_VERSION) $(LSARP_API_PYTHON_BIN_PATH)/pip
 	$(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/pip$(LSARP_API_PYTHON_MAIN_VERSION) install --upgrade --no-cache-dir pip
 	$(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/pip$(LSARP_API_PYTHON_MAIN_VERSION) install pipenv
 	$(SUDO) ln $(PYENV_ROOT)/versions/$(LSARP_API_PYTHON_INSTALL_VERSION)/bin/pipenv $(LSARP_API_PYTHON_BIN_PATH)/pipenv
@@ -22,6 +22,6 @@ pyenv-packages:
 	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 sync-packages:
-	if [ -x /usr/bin/apt-get ]; then sudo pipenv sync --dev; fi
-	if [ -x /usr/bin/yum ];     then pipenv sync --dev; fi
-	if [ -x /usr/local/bin/brew -a `uname` = Darwin ]; then pipenv sync --dev; fi
+	@if [ -x /usr/bin/apt-get ]; then sudo pipenv sync --dev; fi
+	@if [ -x /usr/bin/yum ];     then pipenv sync --dev; fi
+	@if [ -x /usr/local/bin/brew -a `uname` = Darwin ]; then pipenv sync --dev; fi
